c941bf23aa6a513bd6f0920eeebf95ff1e71e4ad
unknown <iced_blaze@web.de>
2015-07-28 22:17:36 +0200

    neu anlegen

---
+++ module/Album/src/Album/Model/Album.php
@@ -0,0 +1,23 @@
+<?PHP
+
+namespace Album\Model;
+
+use ArrayObject;
+/*
+ *
+ */
+class Album extends ArrayObject
+{
+    public $id;
+    public $artist;
+    public $title;
+
+    public function exchangeArray($data)
+    {
+        $this->id     = (!empty($data['id'])) ? $data['id'] : null;
+        $this->artist = (!empty($data['artist'])) ? $data['artist'] : null;
+        $this->title  = (!empty($data['title'])) ? $data['title'] : null;
+    }
+}
+
+?>
